ffc12efe106856e5dc4f42745a1443b02148d8b3
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-05-16 16:37:05 +0100

    Update Menu.js

---
+++ src/components/Menu.js
@@ -0,0 +1,12 @@
+const menu = () => {
+    return (
+        <section>
+            <div className="container">
+                <div className="intro">
+                    <h2 className="text-center">menu</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                </div>
+            </div>
+        </section>
+    )
+}
