63df671d5332198f0a1ada5e6360313abaf5b64e
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-05-16 18:48:33 +0100

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ const menu = () => {
             <div className="container">
                 <div className="intro">
                     <h2 className="text-center">menu</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                    <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
                 </div>
             </div>
         </section>
