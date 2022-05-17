59dc0934bd8e96b1b20d29e5f8be175751bcba07
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-05-17 23:49:26 +0100

    added stuff

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Menu from './components/Menu';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
