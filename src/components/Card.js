0380bb907b6d8b8a3ed2d8420e46bdc56e5b36f4
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-06-01 22:45:47 +0100

    card update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-export default function Card () {
+export default function Card (text, topText) {
     return <div className='card-container'>
-        
+        <div className='img-container'>
+            <img className='img' src='' alt='food image'/>
+        </div>
+        <div className='text-container'>
+            <p className='top-text'>{topText}</p>
+            <p className='text'>{text}</p>
+        </div>
     </div>
 }
--- src/components/MeetComunity.js
+++ src/components/MeetComunity.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Card from './Card';
 
 
 export default function MeetComunity () {
     return <section className='meet-community'>
-        
+        <Card 
+        topText = '@buzzfeedfood'
+        text = 'Lorem ipsum dolor amet mustache knausgaard +1, blue bottle waistcoat tbh semiotics artisan synth stumptown gastropub cornhole celiac swag.'
+        />
     </section>
 }
